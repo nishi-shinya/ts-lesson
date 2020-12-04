23322ca992d42444362fa3db6cab89c2c59f12dc
georgia <georgia6954@gmail.com>
2020-12-04 22:29:55 +0900

    Initialize

---
+++ index.js
@@ -0,0 +1,46 @@
+"use strict";
+let hello = 'hello';
+const fruits = ['Apple', 'Banana', 'Grape'];
+const book = ['Jack', 20, true];
+var CoffeeSize;
+(function (CoffeeSize) {
+    CoffeeSize["SHORT"] = "SHORT";
+    CoffeeSize["TALL"] = "TALL";
+    CoffeeSize["GRANDE"] = "GRANDE";
+})(CoffeeSize || (CoffeeSize = {}));
+const coffee = {
+    hot: true,
+    size: CoffeeSize.SHORT
+};
+let unionType = 10;
+let unionTypes = [10, 'hello'];
+const apple = 'apple';
+let clothSize = 'small';
+function add(num1, num2) {
+    return num1 + num2;
+}
+function sayHello() {
+    console.log('Hello');
+}
+console.log(sayHello());
+let tmp;
+const anotherAdd = add;
+const doubleNumber = num => num * 2;
+function dobleAndHandle(num, cb) {
+    const doubleNum = cb(num * 2);
+    console.log(doubleNum);
+}
+dobleAndHandle(21, doubleNum => {
+    return doubleNum;
+});
+let unknownInput;
+let anyInput;
+let text;
+unknownInput = 'hello';
+if (typeof unknownInput === 'string') {
+    text = unknownInput;
+}
+function error(message) {
+    throw new Error(message);
+}
+console.log(error('this is an error'));
---
+++ index.ts
@@ -0,0 +1,67 @@
+let hello: string = 'hello';
+
+const fruits = ['Apple', 'Banana', 'Grape'];
+
+const book: [string, number, boolean] = ['Jack', 20, true];
+
+
+enum CoffeeSize {
+  SHORT = 'SHORT',
+  TALL = 'TALL',
+  GRANDE = 'GRANDE'
+}
+
+const coffee = {
+  hot: true,
+  size: CoffeeSize.SHORT
+}
+
+let unionType: number | string = 10;
+
+let unionTypes: (number | string)[] = [10,'hello'];
+
+const apple: 'apple' = 'apple'
+
+let clothSize: ClothSize = 'small';
+
+type ClothSize = 'small' | 'medium' | 'large';
+
+function add (num1, num2) {
+  return num1 + num2;
+}
+
+function sayHello (): void {
+  console.log('Hello');
+}
+
+console.log(sayHello());
+
+let tmp: undefined;
+
+const anotherAdd: (n1: number, n2: number) => number = add;
+
+const doubleNumber: (num: number) => number = num => num * 2
+
+function dobleAndHandle(num: number, cb: (num: number) => number ): void {
+  const doubleNum = cb(num * 2);
+  console.log(doubleNum);
+}
+
+dobleAndHandle(21, doubleNum => {
+  return doubleNum;
+})
+
+let unknownInput: unknown;
+let anyInput: any;
+let text: string;
+unknownInput = 'hello';
+
+if (typeof unknownInput === 'string') {
+  text = unknownInput
+}
+
+function error (message: string): never {
+  throw new Error(message);
+}
+
+console.log(error('this is an error'));
