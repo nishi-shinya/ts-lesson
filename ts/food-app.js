1af1a6839cbcf1cd0b2e5f3dc1997601c385bcb2
georgia <georgia6954@gmail.com>
2020-12-06 00:58:05 +0900

    健康管理アプリ　修正

--- ts/food-app.js
+++ ts/food-app.js
@@ -1,8 +1,17 @@
 "use strict";
 class Score {
     get totalScore() {
-        const foods = new Foods();
+        const foods = Foods.getInstance();
         return foods.activeElementsScore.reduce((total, current) => total + current, 0);
+    }
+    render() {
+        document.querySelector('.score__number').textContent = String(this.totalScore);
+    }
+    static getInstance() {
+        if (!Score.instance) {
+            Score.instance = new Score();
+        }
+        return Score.instance;
     }
 }
 class Food {
@@ -12,6 +21,8 @@ class Food {
     }
     clickEventHandler() {
         this.element.classList.toggle('food--active');
+        const score = Score.getInstance();
+        score.render();
     }
 }
 class Foods {
@@ -35,7 +46,7 @@ class Foods {
     }
     get activeElementsScore() {
         this._activeElementsScore = [];
-        this._activeElements.forEach(el => {
+        this.activeElements.forEach(el => {
             const foodScore = el.querySelector('.food__score');
             if (foodScore) {
                 this._activeElementsScore.push(Number(foodScore.textContent));
@@ -44,5 +55,11 @@ class Foods {
         return this._activeElementsScore;
     }
     ;
+    static getInstance() {
+        if (!Foods.instance) {
+            Foods.instance = new Foods();
+        }
+        return Foods.instance;
+    }
 }
-const foods = new Foods();
+const foods = Foods.getInstance();
